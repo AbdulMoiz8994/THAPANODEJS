cb0ae3bf2aa6f5609400b9424aaadf15f9d030d8
AbdulMoiz8994 <moiza8994@gmail.com>
2021-05-05 05:15:15 +0500

    We learnig export and import data from one file to another
    we used modules.exporst={add,names} andfor accessing data to another file we used const varName=require(./path)

---
+++ ImportExport/index.js
@@ -0,0 +1,11 @@
+const {add,sub,names} = require("./open");
+
+// console.log(add(5,5));
+// console.log(add1(5, 10));
+// console.log(sub2(5,5));
+
+
+console.log(add(5, 10));
+console.log(sub(5,5));
+
+console.log(names);
---
+++ ImportExport/open.js
@@ -0,0 +1,19 @@
+const add=(a,b) =>{
+    return a+b
+}
+
+const sub=(a,b) =>{
+    return a-b
+}
+const names="Abdul moiz";
+
+// module.exports.add=add;
+// module.exports.sub=sub;
+
+// it could be any name which we are using with epxorts
+// module.exports.add1=add;
+// module.exports.sub2=sub;
+
+
+// we did the destructure  
+module.exports={add,sub,names}
